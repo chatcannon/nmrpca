b07b365b92b2e82436f6beab369db32d3ccab115
Chris Kerr <cjk34@cam.ac.uk>
2014-07-09 19:08:58 +0100

    The arrays are called W and H in NMF rather than U and V

--- test_nmf.py
+++ test_nmf.py
@@ -22,31 +22,31 @@ def test_NMF_real():
     n_features = 20
     n_components = 3
 
-    U = np.random.rand(n_samples, n_components)
-    V = np.random.rand(n_components, n_features)
+    W = np.random.rand(n_samples, n_components)
+    H = np.random.rand(n_components, n_features)
 
-    # Normalise V
-    Vnorm = np.linalg.norm(V, axis=1)
-    V /= Vnorm[:, None]
+    # Normalise H
+    Hnorm = np.linalg.norm(H, axis=1)
+    H /= Hnorm[:, None]
 
     # Normalise U then order
-    Unorm = np.linalg.norm(U, axis=0)
-    U *= np.arange(n_components, 0, -1) / Unorm
+    Wnorm = np.linalg.norm(W, axis=0)
+    W *= np.arange(n_components, 0, -1) / Wnorm
 
-    X = np.dot(U, V)
+    X = np.dot(W, H)
     assert_array_less(0, X)  # X is strictly greater than 0
 
     p = NMF(n_components, tol=1e-5)
-    Ucalc = p.fit_transform(X)
-    Vcalc = p.components_
-    Xcalc = np.dot(Ucalc, Vcalc)
+    Wcalc = p.fit_transform(X)
+    Hcalc = p.components_
+    Xcalc = np.dot(Wcalc, Hcalc)
 
-    # Check properties of Vcalc and Ucalc
-    assert_sequence_equal(Ucalc.shape, (n_samples, n_components))
-    assert_sequence_equal(Vcalc.shape, (n_components, n_features))
-    assert_array_less(MAX_NEGATIVE_FLOAT, Ucalc)  # Ucalc >= 0
-    assert_array_less(MAX_NEGATIVE_FLOAT, Vcalc)  # Vcalc >= 0
+    # Check properties of Hcalc and Wcalc
+    assert_sequence_equal(Wcalc.shape, (n_samples, n_components))
+    assert_sequence_equal(Hcalc.shape, (n_components, n_features))
+    assert_array_less(MAX_NEGATIVE_FLOAT, Wcalc)  # Wcalc >= 0
+    assert_array_less(MAX_NEGATIVE_FLOAT, Hcalc)  # Hcalc >= 0
 
     assert_array_almost_equal(X, Xcalc, decimal=4)
-#    assert_array_almost_equal(V, Vcalc)
-#    assert_array_almost_equal(U, Ucalc)
+#    assert_array_almost_equal(H, Hcalc)
+#    assert_array_almost_equal(W, Wcalc)
